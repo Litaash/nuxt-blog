73d1956a2f0b70ec42dad032cb34cecfd3a75630
Aleksandr Litash <litaash92@gmail.com>
2021-07-02 15:55:05 +0300

    chapter 4, lesson 1, created admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="admin-layout">
+    <el-container :style="{height: '100%'}">
+      <el-aside width="200px">
+        Aside
+      </el-aside>
+      <el-main>
+        <Nuxt />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .admin-layout {
+    widows: 100%;
+    height: 100vh;
+  }
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Welcome to admin page</h1>
+</template>
+
+<script>
+export default {
+  // render the contents of this component in the admin layout <Nuxt />
+  layout: 'admin'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
