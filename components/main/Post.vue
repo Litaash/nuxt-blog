7020d7df98438118fc29d261bc620f40fa9059f2
Aleksandr Litash <litaash92@gmail.com>
2021-07-02 13:58:59 +0300

    created Post component

---
+++ components/main/Post.vue
@@ -0,0 +1,65 @@
+<template>
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post"
+  >
+    <header slot="header" class="post-header">
+      <h3>Post title</h3>
+
+      <small>
+        <i class="el-icon-time" />
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
+    <div class="post-body">
+      <img
+        src="https://www.tripzaza.com/ru/destinations/files/2017/09/Berlin-e1505798693967.jpg"
+        alt="post image"
+        class="post-image"
+      />
+    </div>
+    <footer class="post-footer">
+      <el-button round @click="openPost">Открыть</el-button>
+
+      <span>
+        <i class="el-icon-message"></i>
+        12
+      </span>
+    </footer>
+  </el-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    openPost() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    margin-bottom: 1.5rem;
+  }
+
+  .post-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .post-image {
+    width: 100%;
+    height: auto;
+  }
+
+  .post-footer {
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
