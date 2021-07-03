7e08b37b398dfa8369c21f2bebe011152332b5b4
Aleksandr Litash <litaash92@gmail.com>
2021-07-03 14:47:58 +0300

    chapter 4, lesson 6, added logout session logic

--- pages/admin/logout.vue
+++ pages/admin/logout.vue
@@ -1,11 +1,15 @@
 <template>
-  <h1>Logout</h1>
+  <div>Очищаем данные</div>
 </template>
 
 <script>
 export default {
   // render the contents of this component in the admin layout <Nuxt />
   layout: 'admin',
-  middleware: ['admin-auth']
+  middleware: ['admin-auth'],
+  beforeCreate() {
+    this.$store.dispatch('auth/logout')
+    this.$router.push('/admin/login?message=logout')
+  }
 }
 </script>
